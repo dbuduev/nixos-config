6b5fc7a61696f81d0b25716a077b2f45309cba5a
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-02-23 19:17:34 +1100

    Add nerd fonts
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- configuration.nix
+++ configuration.nix
@@ -100,6 +100,10 @@ in
   # $ nix search wget
   environment.systemPackages = with pkgs; [ vim wget ghostty ripgrep fd ];
 
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
+  ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
