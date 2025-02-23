b1813255b963ae0b3aa02077a02a18b79479dd4b
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-02-23 17:40:04 +1100

    Initial commit
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "NixOS flake configuration";
+
+  inputs = {
+    # NixOS official package source, using the nixos-24.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        ./configuration.nix
+
+        home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.dennisb = import ./home.nix;
+            home-manager.backupFileExtension = "backup";
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+        }
+      ];
+    };
+  };
+}
