702cbb431b428c7b044953a1581282c68b048125
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-03-03 22:28:37 +1100

    remove atuin
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- home.nix
+++ home.nix
@@ -33,7 +33,6 @@
     jq # A lightweight and flexible command-line JSON processor
     yq-go # yaml processor https://github.com/mikefarah/yq
     eza # A modern replacement for ‘ls’
-    atuin
     bat
 
     # networking tools
