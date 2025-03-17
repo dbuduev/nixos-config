385c22fe606031cbe1a5bb737a61d79841054f81
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-03-17 18:33:09 +1100

    NeoVim: configure go plugin
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- nvim/lua/plugins/go.lua
+++ nvim/lua/plugins/go.lua
@@ -1,5 +1,18 @@
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- Ensure Go parsers are installed
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "go",
+        "gomod",
+        "gosum",
+        "gowork",
+      })
+    end,
+  },
+  {
     "ray-x/go.nvim",
     dependencies = { -- optional packages
       "ray-x/guihua.lua",
@@ -7,10 +20,14 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-      require("go").setup()
+      require("go").setup({
+        treesitter = {
+          install_parsers = true,
+        },
+      })
     end,
     event = { "CmdlineEnter" },
-    ft = { "go", "gomod" },
+    ft = { "go", "gomod", "gosum", "gowork" },
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
 }
