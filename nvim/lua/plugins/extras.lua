049efdde8ccb4b0a7e9754751491250a0311fce9
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-03-20 19:01:14 +1100

    NeoVim: remove rust and zig from extras
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- nvim/lua/plugins/extras.lua
+++ nvim/lua/plugins/extras.lua
@@ -3,8 +3,6 @@ return {
   { import = "lazyvim.plugins.extras.lang.yaml" },
 
   { import = "lazyvim.plugins.extras.lang.go" },
-  { import = "lazyvim.plugins.extras.lang.rust" },
-  { import = "lazyvim.plugins.extras.lang.zig" },
 
   { import = "lazyvim.plugins.extras.lang.nix" }
 }
