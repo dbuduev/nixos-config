85437fa6078f9fed77f4712af3d0095805ef8fa5
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-03-21 22:48:56 +1100

    move extras
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- home/nvim/lua/plugins/extras.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.yaml" },
-
-  { import = "lazyvim.plugins.extras.lang.go" },
-
-  { import = "lazyvim.plugins.extras.lang.nix" }
-}
