c06950985b0e7ec059504e64df19f4c838b138bc
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-04-07 15:17:55 +1000

    Helix: jj to return to normal mode
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- home/home.nix
+++ home/home.nix
@@ -287,8 +287,8 @@
       editor.file-picker.hidden = false;
       editor.file-picker.git-ignore = false;
       keys.insert = {
-        "j" = { j = "normal_mode"; };
-      }
+        "j" = {j = "normal_mode";};
+      };
     };
     languages.language = [
       {
