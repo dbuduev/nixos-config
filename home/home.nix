044329f46f4b0a8fd71b375b3c1fc761583f7b77
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-04-07 15:27:37 +1000

    jk to return
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- home/home.nix
+++ home/home.nix
@@ -287,7 +287,10 @@
       editor.file-picker.hidden = false;
       editor.file-picker.git-ignore = false;
       keys.insert = {
-        "j" = {j = "normal_mode";};
+        "j" = {
+          k = "normal_mode";
+          j = "j";
+        };
       };
     };
     languages.language = [
