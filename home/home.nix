ba57f8def19a6dfd35d3c612801a77ae64620e35
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-04-17 19:06:23 +1000

    nix-ld
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- home/home.nix
+++ home/home.nix
@@ -143,6 +143,15 @@
     };
   };
 
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      stdenv.cc.cc.lib
+      # zlib
+      # openssl
+    ];
+  };
+
   programs.gh = {
     enable = true;
   };
