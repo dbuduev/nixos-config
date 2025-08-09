97ea5d93f1399fa224a015fce2581b572217b8d8
Dennis Buduev <dbuduev@users.noreply.github.com>
2025-08-09 18:19:33 +1000

    emulate x86_64
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- configuration.nix
+++ configuration.nix
@@ -17,6 +17,7 @@ in {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
+  boot.binfmt.emulatedSystems = ["x86_64-linux"];
 
   networking.hostName = "my-nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
