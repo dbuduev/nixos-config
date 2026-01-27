ff7d8565b55d75ca65f454125004c8d42becbdf7
dbuduev <dennis@cerbos.dev>
2026-01-27 20:34:58 +1100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "NixOS flake configuration";
 
   inputs = {
-    # NixOS official package source, using the nixos-25.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -16,25 +15,47 @@
     home-manager,
     ...
   } @ inputs: let
-    system = "aarch64-linux";
-    unstable-pkgs = import nixpkgs-unstable {
-      inherit system;
-      config.allowUnfree = true;
-    };
+    mkSystem = {
+      system,
+      hostName,
+      extraModules ? [],
+    }: let
+      unstable-pkgs = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+      nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {inherit unstable-pkgs;};
+        modules =
+          [
+            ./hosts/${hostName}/configuration.nix
+            ./hosts/${hostName}/hardware-configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = {inherit unstable-pkgs;};
+              home-manager.users.dennisb = import ./home/home.nix;
+              home-manager.backupFileExtension = "backup";
+            }
+          ]
+          ++ extraModules;
+      };
   in {
-    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
-      inherit system;
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.extraSpecialArgs = {inherit unstable-pkgs;};
-          home-manager.users.dennisb = import ./home/home.nix;
-          home-manager.backupFileExtension = "backup";
-        }
-      ];
+    nixosConfigurations = {
+      # VMware VM (ARM64)
+      my-nixos = mkSystem {
+        system = "aarch64-linux";
+        hostName = "vm";
+      };
+
+      # Zenbook S16 (x86_64)
+      zenbook = mkSystem {
+        system = "x86_64-linux";
+        hostName = "zenbook";
+      };
     };
   };
 }
