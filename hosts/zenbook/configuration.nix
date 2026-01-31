43d0b0190d51a815456924115c412179c02456d0
Dennis Buduev <dbuduev@users.noreply.github.com>
2026-01-31 18:53:14 +1100

    disable SSH
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- hosts/zenbook/configuration.nix
+++ hosts/zenbook/configuration.nix
@@ -142,11 +142,10 @@ in {
 
   # List services that you want to enable:
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh.enable = false;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [22];
+  networking.firewall.allowedTCPPorts = [];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
