8e1616de7fe3102951d46f674f1870fa28fd10c6
Dennis Buduev <dbuduev@users.noreply.github.com>
2026-02-01 22:11:06 +1100

    remove dead code
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- hosts/zenbook/configuration.nix
+++ hosts/zenbook/configuration.nix
@@ -235,9 +235,4 @@
       tapping = true;
     };
   };
-
-  # Disable USB wakeup for Logitech Nano Receiver (fixes hibernate resume issues)
-  # services.udev.extraRules = ''
-  #   ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c534", ATTR{power/wakeup}="disabled"
-  # '';
 }
