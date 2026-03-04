036df27def8195cd47d55c04d0b14696823c7c2e
Dennis Buduev <dbuduev@users.noreply.github.com>
2026-03-04 17:54:30 +1100

    no git for coder
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- home/coder.nix
+++ home/coder.nix
@@ -3,16 +3,6 @@
   home.homeDirectory = "/home/coder";
   home.stateVersion = "24.11";
 
-  programs.git = {
-    enable = true;
-    includes = [
-      {
-        condition = "gitdir:/home/shared/projects/cerbos-org/";
-        path = "/home/shared/projects/cerbos-org/.gitconfig";
-      }
-    ];
-  };
-
   home.sessionVariables = {
     DOCKER_HOST = "unix://\${XDG_RUNTIME_DIR}/podman/podman.sock";
   };
