519336ffbfab1bf0bd5ff77e75e3b5813dbf3fb1
Dennis Buduev <dbuduev@users.noreply.github.com>
2026-03-04 18:17:04 +1100

    cc for coder
    Signed-off-by: Dennis Buduev <dbuduev@users.noreply.github.com>

--- home/coder.nix
+++ home/coder.nix
@@ -1,7 +1,15 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  unstable-pkgs,
+  ...
+}: {
   home.username = "coder";
   home.homeDirectory = "/home/coder";
   home.stateVersion = "24.11";
+
+  home.packages = [
+    unstable-pkgs.claude-code
+  ];
 
   home.sessionVariables = {
     DOCKER_HOST = "unix://\${XDG_RUNTIME_DIR}/podman/podman.sock";
